fcc375c9bc14d62fcbaf9d4f0ada6dcbd3b600e8
Erik <ed1783@princeton.edu>
2025-10-31 00:43:26 -0400

    d

--- infinigen/tools/blendscript_path_append.py
+++ infinigen/tools/blendscript_path_append.py
@@ -9,8 +9,3 @@ import sys
 
 pwd = os.getcwd()
 sys.path.append(pwd)
-
-# Also allow a local vendor site-packages folder inside the repo
-vendor_dir = os.path.join(pwd, ".blender_site")
-if os.path.isdir(vendor_dir):
-    sys.path.insert(0, vendor_dir)
